3880539919c73c2256cb0eaf97679f64be782b69
Sachin wele <sachinwele23@gmail.com>
2025-01-14 16:16:48 +0530

    update: sip calculator

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
-} 
+};
+
